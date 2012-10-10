025e39e06e9c093face45158c1ef288f929476f9
Thomas Heller <thom.heller@gmail.com>
2012-10-10 12:20:38 +0200

    Updating .mk scripts to reflect new changes

--- modules/hpx_common.mk
+++ modules/hpx_common.mk
@@ -4,8 +4,15 @@
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-HPX_SRC_ROOT:=$(realpath $(call my-dir)/..)
-include $(HPX_SRC_ROOT)/android-project/boost_common.mk
+HPX_SRC_ROOT:=$(strip $(HPX_SRC_ROOT))
+#/home/heller/programming/boost/trunk
+
+ifndef BOOST_SRC_ROOT
+  $(call __ndk_info,ERROR: You HPX_SRC_ROOT not set)
+  $(call __ndk_info,Please set HPX_SRC_ROOT to point to your Boost source directory and start again.)
+  $(call __ndk_error,Aborting)
+endif
+include ../boost_common.mk
 
 HPX_CPPFLAGS:=-DHPX_COROUTINE_USE_GENERIC_CONTEXT=1
 HPX_CPPFLAGS+=-DBOOST_PARAMETER_MAX_ARITY=7
